f97a701fe606d46df3b90580718a3591da5c2cd2
valatt19 <valatt19@gmail.com>
2021-11-13 12:54:25 +0100

    init 3 pages with topnav

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# visu
+# PROJET VISUALISATION
+
+# Lancer le serveur
+## WINDOWS
+1- pip3 install -r requirements.txt
+2- set FLASK_APP=flask_engine.py
+3- flask run
+
+## LINUX
+1- pip3 install -r requirements.txt
+2- export FLASK_APP=flask_engine.py
+3- flask run
---
+++ app/routes.py
@@ -0,0 +1,28 @@
+from app import app
+from flask import render_template, redirect
+from flask import url_for, request
+
+##########
+# ROUTES #
+##########
+
+# Homepage (for searching a location) ######################
+@app.route("/", methods=["GET","POST"])
+def index():
+    return render_template("homepage.html")
+
+# Visualisation (for  a location) ##########################
+@app.route("/location/", methods=["GET","POST"])
+def location():
+    return render_template("location.html")
+
+# Visualisation (for  a location) ##########################
+@app.route("/comparaison/", methods=["GET","POST"])
+def comparaison():
+    return render_template("comparaison.html")
+
+#######
+# RUN #
+#######
+if __name__ == "__main__":
+        app.run()
