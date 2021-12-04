cc00483293e2344146a94a42f64b5bb68a59c5c7
valatt19 <valatt19@gmail.com>
2021-12-04 17:21:59 +0100

    correct bug erruptions

--- app/static/js/details.js
+++ app/static/js/details.js
@@ -12,30 +12,29 @@ function showDetails(type, index) {
     document.getElementById(eventID).style.display = "contents";
 }
 
-//W3SCHOOL
-var slideIndex = 1;
-showSlides(slideIndex);
+slideIndex = 1
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function plusSlides(n,j) {
+  showSlides(slideIndex += n,j);
 }
 
-function currentSlide(n) {
+function currentSlide(n,j) {
   showSlides(slideIndex = n);
 }
 
-function showSlides(n) {
+function showSlides(n,j) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
+  var v = "mySlides"+j
+  var slides = document.getElementsByClassName(v);
+  //var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";  
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
+  //for (i = 0; i < dots.length; i++) {
+  //    dots[i].className = dots[i].className.replace(" active", "");
+  //}
   slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  //dots[slideIndex-1].className += " active";
 }
